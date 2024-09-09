31ca6d6e6663d4fedeb9e7b5d5511bb83863112a
caio-henrique2006 <caio_henrique2006@outlook.com>
2024-09-09 13:32:51 -0300

    .

---
+++ Arduino/SerialPortNodeJS/index.js
@@ -0,0 +1,11 @@
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const port = new SerialPort({ path:'COM9', baudRate: 9600});
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+// Read the port data
+port.on("open", () => {
+  console.log('serial port open');
+});
+parser.on('data', data =>{
+  console.log(data);
+});
